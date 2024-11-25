76b7bb2f5b34c6a4ef4a0a7654f94bfd190cd5a4
Aislan <aislandnjunior@gmail.com>
2024-11-25 16:47:53 -0300

    ✅ adicionado o useForm para lidar com erros e melhorando a api

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,12 +1,33 @@
-import login from "@/actions/login"
+'use client'
 
-export default async function LoginForm(){
+import login from "@/actions/login"
+import { useFormState, useFormStatus } from "react-dom"
+import Button from "@/components/forms/button"
+
+function FormButton(){
+    const {pending} = useFormStatus()
     return (
         <>
-            <form action={login}>
+        {pending ? <Button disabled={pending}>Enviando...</Button> : <Button>Entrar</Button>}
+        </>
+    )
+}
+
+export default function LoginForm(){
+
+    const [state, action] = useFormState(login, {
+        ok: false,
+        erro: '',
+        data: null,
+    })
+
+    return (
+        <>
+            <form action={action}>
                 <input type="text" name="username" placeholder="Usuário" />
                 <input type="password" name="password" placeholder="Senha" />
-                <button>Entrar</button>
+                <FormButton/>
+                <p>{state.erro}</p>
             </form>
         </>
     )
---
+++ src/functions/apiErro.ts
@@ -0,0 +1,11 @@
+export default function apiError(error: unknown): {
+    data: null;
+    ok: false;
+    erro: string;
+} {
+    if(error instanceof Error){
+        return {data: null, ok: false, erro: error.message}
+    } else{
+        return {data: null, ok:false, erro: 'Erro genérico'}
+    }
+}
