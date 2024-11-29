a448fc7ec53db3bc6f6201bbd8fa7d238a2addcf
Aislan <aislandnjunior@gmail.com>
2024-11-29 20:01:46 -0300

    ✅ adicionado o createContext

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { useUser } from "@/context/user-context"
+
+
 export default async function ContaPage(){
+    const {user} = useUser()
+
+    console.log(user)
     return(
         <main>
-            <h1>Conta</h1>
+            <h1 onClick={() => console.log('Clicou')}>Conta: {user?.nome}</h1>
         </main>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,35 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { UserContextProvider } from "@/context/user-context";
+import userGet from "@/actions/user-get";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
   description: "Projeto do curso NextJS - Origamid",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+  const {data: user} = await userGet()
+
   return (
     <html lang="pt-br">
       <body className={type_second.variable}>
-        <div className='App'>
-          <Header/>
-          <main className="AppBody">
-           {children}
-          </main>
-          <Footer/>
-        </div>
+        <UserContextProvider user={user}>
+          <div className='App'>
+            <Header/>
+            <main className="AppBody">
+            {children}
+            </main>
+            <Footer/>
+          </div>
+        </UserContextProvider>
         
         </body>
     </html>
