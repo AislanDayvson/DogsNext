18daa295a31479be86600b494205fc01430f5787
Aislan <aislandnjunior@gmail.com>
2024-11-29 22:56:47 -0300

    ✅ adicionado o feed da conta

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { PHOTOS_GET } from "@/functions/api";
+import apiError from "@/functions/apiErro";
+
 export type Photo = {
     id: number;
     author: string;
@@ -12,11 +15,23 @@ export type Photo = {
     total_comments: string;
 }
 
-export default async function photosGet(){
-    const response = await fetch('https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0', {
-        next: {revalidate: 10, tags: ['photos']}
-    })
-    const data = await response.json() as Photo[]
+type PhotosGetParams = {
+    page?: number;
+    total?: number;
+    user?: 0 | string; 
+}
 
-    return data
+export default async function photosGet({page = 1, total = 6, user = 0}: PhotosGetParams = {}){
+    try{
+        const {url} = PHOTOS_GET({page, total, user})
+        const response = await fetch(url, {
+        next: {revalidate: 10, tags: ['photos']}
+        })
+        const data = await response.json() as Photo[]
+        if(!response.ok) throw new Error('Erro ao pegar as fotos')
+        return {data, ok: true, erro: ''}
+    } catch (err){
+        return apiError(err)
+    }
+   
 }
--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,5 +1,9 @@
 
+import photosGet from "@/actions/photos-get"
+import userGet from "@/actions/user-get"
+import Feed from "@/components/feed/feed"
 import { Metadata } from "next"
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: 'Minha Conta',
@@ -7,10 +11,16 @@ export const metadata: Metadata = {
 }
 
 export default async function ContaPage(){
+    const {data: user} = await userGet()
+    const {data} = await photosGet({user: user?.username})
 
     return(
         <main>
-            <h1>Conta</h1>
+            {data?.length ? <Feed photos={data} /> : <div>
+                <p style={{color: '#444', fontSize: '1.25rem', marginBottom: '1rem'}}>Nenhuma foto encontrada</p>
+                 <Link href={'/conta/postar'} style={{display: 'inline-block'}} className="button">Postar Foto</Link>
+                 </div>}
+            
         </main>
     )
 }
