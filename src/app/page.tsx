8590758521ab4ff5ad0105374469c66df7d4f026
Aislan <aislandnjunior@gmail.com>
2024-12-01 14:55:39 -0300

    🐛 fix bug na page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Feed from "@/components/feed/feed";
 
 
   export default async function Home() {
-    const data = await photosGet()
-
+    const {data} = await photosGet()
+    if(!data) return null
     return (
       <section className="container mainContainer">
         <Feed photos={data} />
